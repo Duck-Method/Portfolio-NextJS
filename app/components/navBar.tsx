cb12b9f0c433bfc9214967b6507d83f0e1313055
Duck Method <129229008+Duck-Method@users.noreply.github.com>
2025-02-24 16:29:56 -0700

    added portfolio data to data.tsx

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -3,7 +3,7 @@
 
 import { NavLink } from 'react-router-dom';
 
-export default function navBar() {
+export default function NavBar() {
     return(
         <div>
             <NavLink to="/">Home</NavLink>
--- app/lib/data.tsx
+++ app/lib/data.tsx
@@ -4,14 +4,15 @@ export interface ProjectData {
     id: number;
     name: string;
     description: string;
-    url: string;
+    gitUrl: string;
+    liveUrl?: string;
     img: string;
 }
 
 export const project: ProjectData[] = [
-    {id: 1, name: '', description: '',url: '', img: '',},
-    {id: 2, name: '', description: '',url: '', img: '',},
-    {id: 3, name: '', description: '',url: '', img: '',},
-    {id: 4, name: '', description: '',url: '', img: '',},
+    {id: 1, name: 'Pomodoro Timer', description: 'This porject uses NextJS to create a gentle pomodoro timer that creates a soundscape for you to use as background music to complete your tasks.', gitUrl: 'https://github.com/Duck-Method/NextJS-Webpage', liveUrl: 'https://nextjs-webpage-hlm5.onrender.com/', img: 'Portfolio-NextJS/public/pomodoroTimer.png',},
+    {id: 2, name: 'Healers Art', description: 'This is the first website I created on my own from scratch. It uses very basic HTML and CSS. I keep this one around to remind me how far I have come in the complexity of what I can make.', gitUrl: 'https://github.com/Duck-Method/Healers-Art', liveUrl: 'https://healersartmassage.com', img: 'Portfolio-NextJS/public/healArt.png',},
+    {id: 3, name: 'OOP Vehicle Builder', description: 'This is an example of Object Oriented Programming using a Typescript and modular structure to create a CLI that walks you through the various option provided for creating a car, truck, or motorbike object and performing actions on that object.', gitUrl: 'https://github.com/Duck-Method/OOP-Vehicle-Builder', liveUrl: 'https://drive.google.com/file/d/10jZJw71G-4NQb-ykSvepndjinmtGVozv/view?usp=sharing', img: 'Portfolio-NextJS/public/OOPVehicleBuilder.png',},
+    {id: 4, name: 'Hyrule Compendium', description: 'This is a group project that was created during the coding bootcamp I attended. It was my responsibility to create the front-end components for populating the categories, page info, comments, and reactions based upon the API calls to the server. The information is updated dynamically based upon what is available on the server, so as users add pages, those pages are available within the navigation automatically.', gitUrl: 'https://github.com/Cinnlight/Hyrule-Compendium', liveUrl: 'https://hyrule-compendium.magicapps.dev/', img: 'Portfolio-NextJS/public/hyruleCompendium.png',},
     //{id: , name: '', description: '',url: '', img: '',}, // copy this line to create new structure and then uncomment for use
 ]
