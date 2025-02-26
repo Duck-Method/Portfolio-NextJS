887d50e0f3e7ffd2c9c4269623210df5d2efe50a
Duck Method <129229008+Duck-Method@users.noreply.github.com>
2025-02-26 00:15:38 -0700

    updated function and css for project page. still needs adjustments

--- app/projects/portfolioCard.tsx
+++ app/projects/portfolioCard.tsx
@@ -1,24 +1,46 @@
 'use client';
 import { project } from "../lib/data";
 import styles from './ProjectCard.module.css';
+import { useState } from 'react';
+import Image from 'next/image';
 
 export default function ProjectCard() {
+    const [selectedId, setSelectedId] = useState<number | null>(null);
+
+    const handleClick = (id: number) => {
+        setSelectedId((prevId) => (prevId === id ? null : id));
+    }
 
     return (
-        <section className={styles.section}>
+        <>
             {project.map((proj) => (            
-                <a key={proj.id} 
-                    href={proj.liveUrl} 
-                    className={styles.card} 
-                    style={{ 
-                        backgroundImage: `url(${proj.img})`
-                        }}>
-                    <h3 className=''>
+                <div key={proj.id} className={styles.section} onClick={() => handleClick(proj.id)}>
+                    <h3>
                         {proj.name}
                     </h3>
-                    <p className=''>{proj.description}</p>
-                </a>
+
+                    {selectedId === proj.id && (                    
+                        <div className={styles.card}>
+                            <Image 
+                                src={proj.img}  
+                                alt={proj.name}
+                                width={300}
+                                height={300}
+                                className={styles.image} 
+                            /> 
+                            <div>
+                                <a href={proj.liveUrl}>
+                                    View Live
+                                </a>
+                                <a href={proj.gitUrl}>
+                                    View Code
+                                </a>
+                            </div>
+                            <p>{proj.description}</p>
+                        </div>
+                    )}
+                </div>
             ))}    
-        </section>
+        </>
     );
 }
