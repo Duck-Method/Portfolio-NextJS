3d76c1090f1b146d7225299186e52902ad06ede2
Duck Method <129229008+Duck-Method@users.noreply.github.com>
2025-02-27 15:12:12 -0700

    added functionality to nave bar to indicate what page is selected within nav bar.

--- app/components/navBar.tsx
+++ app/components/navBar.tsx
@@ -1,17 +1,18 @@
 // /components/navBar.tsx
 'use client';
 import styles from './navBar.module.css';
-
+import { usePathname } from 'next/navigation'
 import Link from 'next/link';
 
 export default function NavBar() {
+    const pathname = usePathname();
     return(
         <div className={styles.navBar}>
-            <Link href="/">Home</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/about">About Me</Link>
-            <Link href="/contact">Contact</Link>
-            <Link href="/resume">Resume</Link>
+            <Link className={`${pathname === "/" ? styles.active : ""}`} href="/">Home</Link>
+            <Link className={`${pathname === "/projects" ? styles.active : ""}`} href="/projects">Projects</Link>
+            <Link className={`${pathname === "/about" ? styles.active : ""}`} href="/about">About Me</Link>
+            <Link className={`${pathname === "/contact" ? styles.active : ""}`} href="/contact">Contact</Link>
+            <Link className={`${pathname === "/resume" ? styles.active : ""}`} href="/resume">Resume</Link>
         </div>
     )
 }
