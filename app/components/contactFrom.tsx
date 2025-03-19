e30d141582a5553f569bb9b05525e2005f3ddaf4
Brian <129229008+Duck-Method@users.noreply.github.com>
2025-03-18 20:23:56 -0600

    added about me page and styled accordingly. Contact form component commented out for testing

--- app/components/contactFrom.tsx
+++ app/components/contactFrom.tsx
@@ -1,7 +1,7 @@
 // /components/contactFrom.tsx
 'use client';
 
-import { useForm, SubmitHandler } from 'react-hook-form';
+// import { useForm, SubmitHandler } from 'react-hook-form';
 
 type FormData = {
     name: string;
@@ -11,22 +11,22 @@ type FormData = {
 
 export default function ContactForm() {
 
-    const { register, handleSubmit, formState: { errors } } =useForm<FormData>();
+    // const { register, handleSubmit, formState: { errors } } =useForm<FormData>();
 
-    const onSubmit: SubmitHandler<FormData> = (data) => {
-        console.log('Form submitted:', data);
-    };
+    // const onSubmit: SubmitHandler<FormData> = (data) => {
+    //     console.log('Form submitted:', data);
+    //};
 
     return (
         <form>
-            <input {...register('name', {required: true})} type='text' placeholder='Name'>
-                {errors.name && <span>This field is required</span>}
-            </input>
-            <input {...register('email', {required: true, pattern: /^\S+@\S+$/i})} type='email' placeholder='Email'>
-                {errors.email && <span>Invalid email address</span>}
-            </input>
-            <textarea {...register('message', {required: true})} placeholder='Message Here' >
-            </textarea>
+            {/* <input {...register('name', {required: true})} type='text' placeholder='Name'> */}
+                {/* {errors.name && <span>This field is required</span>} */}
+            {/* </input> */}
+            {/* <input {...register('email', {required: true, pattern: /^\S+@\S+$/i})} type='email' placeholder='Email'> */}
+                {/* {errors.email && <span>Invalid email address</span>} */}
+            {/* </input> */}
+            {/* <textarea {...register('message', {required: true})} placeholder='Message Here' > */}
+            {/* </textarea> */}
             <button type='submit'>
                 Send
             </button>
